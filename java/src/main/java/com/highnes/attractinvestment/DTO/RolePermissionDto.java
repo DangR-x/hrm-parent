36d7c15791dfff8d55ec3e5f4295bf41124ef6b4
傅良云 <fuliangyun@highnes.com>
2020-07-01 19:09:50 +0800

    7.1

---
+++ java/src/main/java/com/highnes/attractinvestment/DTO/RolePermissionDto.java
@@ -0,0 +1,9 @@
+package com.highnes.attractinvestment.DTO;
+
+/**
+ * @author Rain
+ * @date 2020/7/1
+ * @description
+ */
+public class RolePermissionDto {
+}
---
+++ java/src/main/java/com/highnes/attractinvestment/VO/RolePermissionVO.java
@@ -0,0 +1,9 @@
+package com.highnes.attractinvestment.VO;
+
+/**
+ * @author Rain
+ * @date 2020/7/1
+ * @description
+ */
+public class RolePermissionVO {
+}

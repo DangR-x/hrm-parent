dcdd444b4321f00268f8fe0c825b57d44ae1313f
DangR-X <1023056792@qq.com>
2020-07-24 14:56:10 +0800

    DangR-X

--- java/src/main/java/com/highnes/attractinvestment/DTO/RolePermissionDto.java
+++
@@ -1,9 +0,0 @@
-package com.highnes.attractinvestment.DTO;
-
-/**
- * @author Rain
- * @date 2020/7/1
- * @description
- */
-public class RolePermissionDto {
-}
--- java/src/main/java/com/highnes/attractinvestment/VO/RolePermissionVO.java
+++
@@ -1,9 +0,0 @@
-package com.highnes.attractinvestment.VO;
-
-/**
- * @author Rain
- * @date 2020/7/1
- * @description
- */
-public class RolePermissionVO {
-}
